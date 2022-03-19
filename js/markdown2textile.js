a93ed2ed47d4dd3da10640e9e82c6fbb8265d562
eycjur <eycjur@users.noreply.github.com>
2022-03-19 11:31:46 +0000

    deploy: cb5d92cb80dfb7bbcdfc5e1096c5515b06f9dbd9

--- js/markdown2textile.js
+++ js/markdown2textile.js
@@ -1,19 +1,19 @@
-import { markdown2xml, xml2TextileForNotion } from './convert.js';
+import { markdown2xml, xml2textile, xmlCareForNotion } from "./convert.js";
 function btnClick(obj = NaN) {
-    let elemMarkdown = document.getElementById("markdown-text");
-    let elemTextile = document.getElementById("textile-text");
+    const elemMarkdown = (document.getElementById("markdown-text"));
+    const elemTextile = (document.getElementById("textile-text"));
     elemTextile.value = "";
-    let markdownText = elemMarkdown.value;
-    let textileText = xml2TextileForNotion(markdown2xml(markdownText));
+    const markdownText = elemMarkdown.value;
+    const textileText = xml2textile(xmlCareForNotion(markdown2xml(markdownText)));
     elemTextile.value = textileText;
     elemMarkdown.value = "";
     navigator.clipboard.writeText(textileText);
 }
-document.addEventListener('DOMContentLoaded', function () {
-    let elemMarkdown = document.getElementById("markdown-text");
-    let button = document.getElementById("convert-button");
+document.addEventListener("DOMContentLoaded", function () {
+    const elemMarkdown = (document.getElementById("markdown-text"));
+    const button = (document.getElementById("convert-button"));
     button.addEventListener("click", btnClick);
-    elemMarkdown.addEventListener('keydown', function (event) {
+    elemMarkdown.addEventListener("keydown", function (event) {
         if (event.key == "Enter" && event.ctrlKey) {
             btnClick();
         }
